ff348dce8630a211bf2912ea862e9ca9f350d515
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2019-09-28 16:07:15 +0200

    Removed pretrained RelGAN generator path

--- run/run_relgan.py
+++ run/run_relgan.py
@@ -124,7 +124,7 @@ args = [
     '--num_heads', num_heads,
     '--head_size', head_size,
     '--gen_pretrain', int(True),
-    '--pretrained_gen_path', 'pretrain/trump_tweets_2009-2019/gen_MLE_pretrain_relgan_vanilla_sl75_sn10000.pt',
+    # '--pretrained_gen_path', 'pretrain/trump_tweets_2009-2019/gen_MLE_pretrain_relgan_vanilla_sl75_sn10000.pt',
 
     # Discriminator
     '--adv_d_step', ADV_d_step,
